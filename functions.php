f469017a35269d158e990d60864ff63027615b83
TOMOKI922 <80738493+TOMOKI922@users.noreply.github.com>
2021-04-02 13:57:43 +0900

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function new_excerpt_mblength($length) {
 }
 add_filter('excerpt_mblength', 'new_excerpt_mblength');
 function new_excerpt_more($more) {
-  return ' [続きを読む]';
+  return '<br>[続きを読む]';
 }
 add_filter('excerpt_more', 'new_excerpt_more');
 function search_template_redirect(){
